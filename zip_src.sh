0620424f646131cc99b0e2294cef25f7b8939917
Michael Swart <mswart@princeton.edu>
2019-04-05 11:56:30 -0400

    explain colors and do not upload search.js

--- zip_src.sh
+++ zip_src.sh
@@ -2,6 +2,6 @@
 
 rm src.zip
 cd src
-FILES=$(ls -p | grep -v /)
+FILES=$(ls -p | grep -v / | grep -v "search.js")
 echo $FILES
 zip -r ../src.zip $FILES logos/* modules/mutation-summary/src modules/crypto-js/* modules/jsencrypt.min.js
