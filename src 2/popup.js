adc022a17b467b38b670e551f939a5d19265db19
Michael Swart <mswart@princeton.edu>
2019-07-09 15:33:27 -0700

    remove folder

--- src 2/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("open-options").addEventListener("click", openOptions);
-
-function openOptions() {
-	if (chrome.runtime.openOptionsPage) {
-		chrome.runtime.openOptionsPage();
-	} else {
-		window.open(chrome.runtime.getURL('options.html'));
-	}
-}
